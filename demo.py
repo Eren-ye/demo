703ab3b81ee2f312efb17c8a5d0c97d50773b3f7
lordd6171@gmail.com <gyomei987@outlook.com>
2024-07-09 17:01:23 +0545

    UPDATE

--- demo.py
+++ demo.py
@@ -39,12 +39,31 @@
 # else:
 #     print(" grade F")
 
-age=int(input(" Enter your age: "))
-if age<18:
-    print(" you are child")
-elif age>=18:
-    if age>=18 and age<=60:
-        print(" you are eligible to vote")
-    else:
-        print(" You are old")
+# age=int(input(" Enter your age: "))
+# if age<18:
+#     print(" you are child")
+# elif age>=18:
+#     if age>=18 and age<=60:
+#         print(" you are eligible to vote")
+#     else:
+#         print(" You are old")
 
+pin=int(input("Enter the PIN :"))
+if pin==1234:
+    print(" Welcome to ATM")
+    print(" 1. Balance Enquiry")
+    print(" 2. Withdrawal")
+    option=int(input("Enter your option :"))
+    if option==1:
+        print(" Your balance is 10000")
+    elif option==2:
+        print(" Enter the amount to withdraw :")
+        amount=int(input("Enter the amount :"))
+        if amount>10000:
+            print(" Insufficient balance")
+        else:
+            print(f" Your preveious balance is 10000 and current amount is {10000-amount}")
+    else:
+        print(" Invalid option")
+else:
+    print(" Invalid PIN")
