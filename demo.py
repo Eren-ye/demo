056c06ae8363b9190bb4d6e1571ed5dd9a63d752
lordd6171@gmail.com <gyomei987@outlook.com>
2024-07-09 17:09:38 +0545

    UPDATE

--- demo.py
+++ demo.py
@@ -48,22 +48,3 @@
 #     else:
 #         print(" You are old")
 
-pin=int(input("Enter the PIN :"))
-if pin==1234:
-    print(" Welcome to ATM")
-    print(" 1. Balance Enquiry")
-    print(" 2. Withdrawal")
-    option=int(input("Enter your option :"))
-    if option==1:
-        print(" Your balance is 10000")
-    elif option==2:
-        print(" Enter the amount to withdraw :")
-        amount=int(input("Enter the amount :"))
-        if amount>10000:
-            print(" Insufficient balance")
-        else:
-            print(f" Your preveious balance is 10000 and current amount is {10000-amount}")
-    else:
-        print(" Invalid option")
-else:
-    print(" Invalid PIN")
